04229930a6adcbe7b0f01c937267e0e875c569a7
Tewann <tewann@gmail.com>
2018-06-08 20:48:23 +0200

    Add component ContactsScreen

---
+++ src/Components/ContactsScreen/ContactsScreen.js
@@ -0,0 +1,19 @@
+//src/Components/ContactsScreen/ContactsScreen.js
+//main screen for contacts
+
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class ContactsScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    ContactsScreen
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default ContactsScreen
