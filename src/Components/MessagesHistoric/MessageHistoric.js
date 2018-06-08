b2923bf139c48645e0eabd95d0bc01f59ac8303f
Tewann <tewann@gmail.com>
2018-06-08 20:55:02 +0200

    Add component MessagesHistoric

---
+++ src/Components/MessagesHistoric/MessageHistoric.js
@@ -0,0 +1,19 @@
+//src/Components/MessagesHistoric/MessagesHistoric.js
+//main screen for messages history
+
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class MessagesHistoric extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    MessagesHistoric
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default MessagesHistoric
