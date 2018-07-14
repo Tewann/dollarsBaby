cd98ce611f46e8d844d917bbd72d290689ee72ac
Tewann <tewann@gmail.com>
2018-07-14 16:52:45 +0200

    Profil Screen improved

--- android/app/src/main/java/com/eblink/MainApplication.java
+++ android/app/src/main/java/com/eblink/MainApplication.java
@@ -3,6 +3,7 @@ package com.eblink;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNFirebasePackage(),
