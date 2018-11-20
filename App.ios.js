162944d3fe4b4330fd6b14265ad49926fa1f6785
Tewann <tewann@gmail.com>
2018-11-20 21:35:02 +0100

    Splash screen android added (with react-native-splash-screen) / App.android.js (without safeviewarea) App.ios.js added

--- App.ios.js
+++ App.ios.js
@@ -8,6 +8,7 @@ import firebase from 'react-native-firebase'
 import Dimensions from 'Dimensions'
 import { SafeAreaView } from 'react-navigation'
 import { Platform } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
 
 const { height, width } = Dimensions.get('window');
 const heightString = Math.trunc(height).toString()
@@ -18,6 +19,10 @@ const request = new AdRequest();
 const unitID = Platform.OS === 'ios' ? 'ca-app-pub-4868408770331668/3443399374' : "ca-app-pub-3940256099942544/6300978111"
 
 export default class App extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide()
+  }
+  
   render() {
     let persistor = persistStore(Store)
     return (
